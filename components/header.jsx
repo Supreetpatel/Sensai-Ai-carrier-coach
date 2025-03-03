c4e2d9a7455867ca89ba9894280d9bf705be0278
Supreet0 <supreetpatel2004@gmail.com>
2025-03-03 15:20:50 +0530

    Refactored the design of the page and also made protection to the page

--- components/header.jsx
+++ components/header.jsx
@@ -31,39 +31,50 @@ const Header = () => {
         <div className="flex items-center space-x-2 md:space-x-4">
           <SignedIn>
             <Link href={"/dashboard"}>
-              <Button>
+              <Button variant="outline">
                 <LayoutDashboard className="h-4 w-4" />
                 <span className="hidden md:block">Industry Insights</span>
               </Button>
             </Link>
+            <DropdownMenu>
+              <DropdownMenuTrigger>
+                <Button>
+                  <StarsIcon className="h-4 w-4" />
+                  <span className="hidden md:block">Growth Tools</span>
+                  <ChevronDown className="h-4 w-4" />
+                </Button>
+              </DropdownMenuTrigger>
+              <DropdownMenuContent>
+                <DropdownMenuItem>
+                  <Link href="">
+                    <FileText className="h-4 w-4" />
+                    <span>Growth Tools</span>
+                  </Link>
+                </DropdownMenuItem>
+                <DropdownMenuItem>Billing</DropdownMenuItem>
+                <DropdownMenuItem>Team</DropdownMenuItem>
+              </DropdownMenuContent>
+            </DropdownMenu>
           </SignedIn>
-          <DropdownMenu>
-            <DropdownMenuTrigger>
-              <Button>
-                <StarsIcon className="h-4 w-4" />
-                <span className="hidden md:block">Growth Tools</span>
-                <ChevronDown className="h-4 w-4" />
-              </Button>
-            </DropdownMenuTrigger>
-            <DropdownMenuContent>
-              <DropdownMenuItem>
-                <Link href="">
-                  <FileText className="h-4 w-4" />
-                  <span>Growth Tools</span>
-                </Link>
-              </DropdownMenuItem>
-              <DropdownMenuItem>Billing</DropdownMenuItem>
-              <DropdownMenuItem>Team</DropdownMenuItem>
-            </DropdownMenuContent>
-          </DropdownMenu>
+          <SignedOut>
+            <SignInButton>
+              <Button variant="outline">Sign In</Button>
+            </SignInButton>
+          </SignedOut>
+          <SignedIn>
+            <UserButton
+              appearance={{
+                elements: {
+                  avatarBox: "w-10 h-10",
+                  userButtonPopoverCard: "shadow-xl",
+                  userPreviewMainIdentifier: "font-semibold",
+                },
+              }}
+              afterSignOutUrl="/"
+            />
+          </SignedIn>
         </div>
       </nav>
-      <SignedOut>
-        <SignInButton />
-      </SignedOut>
-      <SignedIn>
-        <UserButton />
-      </SignedIn>
     </header>
   );
 };
